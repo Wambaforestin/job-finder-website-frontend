d686ed9b6b1dd9f88324c8dc64b2ea2e97062426
Wambaforestin <wambaforestin@gmail.com>
2024-04-05 12:33:14 +0200

    Update login links to use login_register.html

--- JS/script.js
+++ JS/script.js
@@ -14,7 +14,7 @@ window.onscroll = () => {
 
 document.querySelectorAll('input[type="number"]').forEach((input) => {
     inputNumber.ouinput = () => {
-        if(inputNumber.value.length > inputNumber.maxLength) {
+        if (inputNumber.value.length > inputNumber.maxLength) {
             inputNumber.value = inputNumber.value.slice(0, inputNumber.maxLength);
         };
     }
@@ -23,14 +23,33 @@ document.querySelectorAll('input[type="number"]').forEach((input) => {
 //for the animated send message button in the contact form
 
 let btn = document.getElementById('btn');
-btn.addEventListener("click", function() {
-  btn.setAttribute('class', 'submit process');
-  btn.innerHTML = 'Processing';
-  setTimeout(()=>{
-     btn.setAttribute('class', 'submit submitted');
-     btn.innerHTML = `
+btn.addEventListener("click", function () {
+    btn.setAttribute('class', 'submit process');
+    btn.innerHTML = 'Processing';
+    setTimeout(() => {
+        btn.setAttribute('class', 'submit submitted');
+        btn.innerHTML = `
      <span class="tick">&#10004;</span>
      Submitted
      `;
-  },5000);
+    }, 5000);
 });
+
+//for the animated sign and sign up form in the login_register page
+const loginText = document.querySelector(".title-text .login");
+const loginForm = document.querySelector("form.login");
+const loginBtn = document.querySelector("label.login");
+const signupBtn = document.querySelector("label.signup");
+const signupLink = document.querySelector("form .signup-link a");
+signupBtn.onclick = () => {
+    loginForm.style.marginLeft = "-50%";
+    loginText.style.marginLeft = "-50%";
+};
+loginBtn.onclick = () => {
+    loginForm.style.marginLeft = "0%";
+    loginText.style.marginLeft = "0%";
+};
+signupLink.onclick = () => {
+    signupBtn.click();
+    return false;
+};
