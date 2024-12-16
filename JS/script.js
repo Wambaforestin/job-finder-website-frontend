a84c675d18853eaf39c2259bee70e2410c9d16c5
Wambaforestin <wambaforestin@gmail.com>
2024-12-16 01:05:58 +0100

    Refactor navbar functionality and update CSS file paths in HTML files

--- JS/script.js
+++ JS/script.js
@@ -1,55 +1,57 @@
-let navbar = document.querySelector('.header .flex .navbar');
+document.addEventListener('DOMContentLoaded', () => {
+    let navbar = document.querySelector('.header .flex .navbar');
 
-document.querySelector('#menu-btn').onclick = () => {
-    navbar.classList.toggle('active');
-}
-
-// Remove the active class from the navbar when the user scrolls ✌️
-
-window.onscroll = () => {
-    navbar.classList.remove('active');
-}
-
-//The code below is for the input number to not exceed the max length of the input
-
-document.querySelectorAll('input[type="number"]').forEach((input) => {
-    input.oninput = () => {
-        if (input.value.length > input.maxLength) {
-            input.value = input.value.slice(0, input.maxLength);
-        }
+    document.querySelector('#menu-btn').onclick = () => {
+        navbar.classList.toggle('active');
     }
-});
 
-//for the animated send message button in the contact form
+    // Remove the active class from the navbar when the user scrolls ✌️
 
-let btn = document.getElementById('btn');
-btn.addEventListener("click", function () {
-    btn.setAttribute('class', 'submit process');
-    btn.innerHTML = 'Processing';
-    setTimeout(() => {
-        btn.setAttribute('class', 'submit submitted');
-        btn.innerHTML = `
+    window.onscroll = () => {
+        navbar.classList.remove('active');
+    }
+
+    //The code below is for the input number to not exceed the max length of the input
+
+    document.querySelectorAll('input[type="number"]').forEach((input) => {
+        input.oninput = () => {
+            if (input.value.length > input.maxLength) {
+                input.value = input.value.slice(0, input.maxLength);
+            }
+        }
+    });
+
+    //for the animated send message button in the contact form
+
+    let btn = document.getElementById('btn');
+    btn.addEventListener("click", function () {
+        btn.setAttribute('class', 'submit process');
+        btn.innerHTML = 'Processing';
+        setTimeout(() => {
+            btn.setAttribute('class', 'submit submitted');
+            btn.innerHTML = `
      <span class="tick">&#10004;</span>
      Submitted
      `;
-    }, 5000);
-});
+        }, 5000);
+    });
 
-//for the animated sign and sign up form in the login_register page
-const loginText = document.querySelector(".title-text .login");
-const loginForm = document.querySelector("form.login");
-const loginBtn = document.querySelector("label.login");
-const signupBtn = document.querySelector("label.signup");
-const signupLink = document.querySelector("form .signup-link a");
-signupBtn.onclick = () => {
-    loginForm.style.marginLeft = "-50%";
-    loginText.style.marginLeft = "-50%";
-};
-loginBtn.onclick = () => {
-    loginForm.style.marginLeft = "0%";
-    loginText.style.marginLeft = "0%";
-};
-signupLink.onclick = () => {
-    signupBtn.click();
-    return false;
-};
+    //for the animated sign and sign up form in the login_register page
+    const loginText = document.querySelector(".title-text .login");
+    const loginForm = document.querySelector("form.login");
+    const loginBtn = document.querySelector("label.login");
+    const signupBtn = document.querySelector("label.signup");
+    signupBtn.onclick = () => {
+        loginForm.style.marginLeft = "-50%";
+        loginText.style.marginLeft = "-50%";
+    };
+    loginBtn.onclick = () => {
+        loginForm.style.marginLeft = "0%";
+        loginText.style.marginLeft = "0%";
+    };
+    signupLink.onclick = () => {
+        signupBtn.click();
+        return false;
+    };
+}); return false;
+
